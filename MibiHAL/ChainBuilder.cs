59fcbda003e69f4da6fe51ea810a0c75752dcea1
Chris Whitworth <chris.whitworth@red-gate.com>
2014-11-21 09:52:36 +0000

    Refactor a few things

---
+++ MibiHAL/ChainBuilder.cs
@@ -0,0 +1,59 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MibiHAL
+{
+    class ChainBuilder
+    {
+        private readonly Brain m_Brain;
+        private readonly Random m_Random = new Random();
+
+        public ChainBuilder(Brain brain)
+        {
+            m_Brain = brain;
+        }
+
+        public Chain BuildChain(Chain starter)
+        {
+            var output = starter.First(1);
+
+            do
+            {
+                var candidates = m_Brain.Candidates(starter);
+
+                var chain = PickChain(candidates);
+
+                output = output.Join(chain.Last(chain.Length - 1));
+
+                starter = chain.Last(1);
+            
+            } while (!starter.Symbols.Last().Terminal);
+
+            return output;
+        }
+
+        private Chain PickChain(IEnumerable<Chain> candidates)
+        {
+            var chains = candidates as Chain[] ?? candidates.ToArray();
+            var scores = chains.Select(m_Brain.Score).ToArray();
+            var total = scores.Aggregate(0, (i1, i2) => i1 + i2);
+
+            var c = m_Random.Next(total);
+
+            var s = 0;
+            Chain chain = null;
+            for (int j = 0; j < scores.Count(); j++)
+            {
+                s += scores[j];
+                if (s > c)
+                {
+                    chain = chains[j];
+                    break;
+                }
+            }
+            if (chain == null) chain = chains.Last();
+            return chain;
+        }
+    }
+}
--- MibiHAL/Program.cs
+++ MibiHAL/Program.cs
@@ -9,10 +9,35 @@ namespace MibiHAL
     {
         static void Main(string[] args)
         {
-            string line;
-            var set = new HashSet<ISymbol>();
             var b = new Brain();
-            var file = new StreamReader("train.txt");
+            var r = new Random();
+
+            var chainBuilder = new ChainBuilder(b);
+
+            var set = Train(b, "train.txt");
+
+
+            var words = set.ToArray();
+
+            Console.WriteLine("Trained!");
+
+            var starterWord = words[r.Next(words.Length)];
+            
+            for (int i = 0; i < 10; i++)
+            {
+                var starter = new Chain(starterWord);
+
+                var output = chainBuilder.BuildChain(starter);
+
+                Console.WriteLine(string.Join("", output.Symbols));
+            }
+        }
+
+        private static IEnumerable<ISymbol> Train(Brain b, string trainFile)
+        {
+            var set = new HashSet<ISymbol>();
+            var file = new StreamReader(trainFile);
+            string line;
             while ((line = file.ReadLine()) != null)
             {
                 if (line.StartsWith("#")) continue;
@@ -27,57 +52,7 @@ namespace MibiHAL
                 b.Train(symbols, 3);
                 b.Train(symbols, 5);
             }
-
-            var r = new Random();
-
-            var words = set.ToArray();
-
-            Console.WriteLine("Trained!");
-
-            var starterWord = words[r.Next(words.Length)];
-            for (int i = 0; i < 10; i++)
-            {
-                var starter = new Chain(starterWord);
-
-                var output = starter.First(1);
-
-                do
-                {
-                    var candidates = b.Candidates(starter);
-
-                    var chains = candidates as Chain[] ?? candidates.ToArray();
-
-                    var scores = chains.Select(b.Score).ToArray();
-                    var total = scores.Aggregate(0, (i1, i2) => i1 + i2);
-
-                    var c = r.Next(total);
-
-                    var s = 0;
-                    Chain chain = null;
-                    for (int j = 0; j < scores.Count(); j++)
-                    {
-                        s += scores[j];
-                        if (s > c)
-                        {
-                            chain = chains[j];
-                            break;
-                        }
-                    }
-                    if (chain == null) chain = chains.Last();
-
-
-                    output = output.Join(chain.Last(chain.Length - 1));
-
-                    starter = chain.Last(1);
-
-                } while (!starter.Symbols.Last().Terminal);
-
-
-
-                Console.WriteLine(string.Join("", output.Symbols));
-
-
-            }
+            return set;
         }
     }
 }
